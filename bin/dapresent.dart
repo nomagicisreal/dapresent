229e51068aa4354342b3434a066d3aead395c3d1
nomisal <tryeverythingican@gmail.com>
2024-02-23 22:56:57 +0800

    Initial commit

---
+++ bin/dapresent.dart
@@ -0,0 +1,5 @@
+import 'package:dapresent/dapresent.dart' as dapresent;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dapresent.calculate()}!');
+}
---
+++ test/dapresent_test.dart
@@ -0,0 +1,8 @@
+import 'package:dapresent/dapresent.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
