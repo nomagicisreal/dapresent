61253d73ee060e0113a0a99bbfaaae7382263c2c
nomisal <tryeverythingican@gmail.com>
2025-05-03 23:44:22 +0800

    2025-05-03 clean

--- lib/src_material/rendering/rendering.dart
+++ lib/src_material/rendering/rendering.dart
@@ -1,10 +1,7 @@
 library;
 
-import 'dart:math' as math;
 import 'package:damath/damath.dart';
-import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
 
 part 'render.dart';
 part 'gesture/grid_2d.dart';
